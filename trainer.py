e706bff74fea0fb19607cc938793eb36323bf3bb
macarious <63441014+macarious@users.noreply.github.com>
2024-04-02 10:55:55 -0700

    Add starter code for training

---
+++ trainer.py
@@ -0,0 +1,7 @@
+'''
+Author: HUI, Macarious
+
+This file is used to train a model that can predict where speech errors occur in an audio file.
+Podcast audio files are used as training data, and they are manually labeled with the location of speech errors.
+'''
+
