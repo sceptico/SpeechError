3ff929acd1ed5b3b54dc291d8ca76f0cb03fe695
macarious <63441014+macarious@users.noreply.github.com>
2024-06-24 01:22:01 -0700

    Revise feature extraction script to create phrase-level arrays

--- scripts/generate_features.sh
+++ scripts/generate_features.sh
@@ -1,7 +1,8 @@
 PROCESS_NUM=20
 WAVE_LIST=data/metadata/wav_list.lst
 WAVE_DIR=data/audio
+TRANSCRIPT_DIR=data/whisperX
 FEATURE_DIR=data/feature
-FEATURE_CONFIG=feature_extraction/feature.cfg
+FEATURE_CONFIG=code/feature_extraction/feature.cfg
 
-python feature_extraction/generate_features.py --wav_list $WAVE_LIST --wav_dir $WAVE_DIR --feature_dir $FEATURE_DIR --feature_config $FEATURE_CONFIG --n_process $PROCESS_NUM
+python code/feature_extraction/generate_features.py --wav_list $WAVE_LIST --wav_dir $WAVE_DIR --transcript_dir $TRANSCRIPT_DIR --feature_dir $FEATURE_DIR --feature_config $FEATURE_CONFIG --n_process $PROCESS_NUM
